c4ab81e592c7a35fe28232208f9693be970cc4fc
f-mtz <mtz.dev.si@gmail.com>
2020-03-08 03:21:18 -0300

    list of transactions

--- src/application/bank/AppCaixaEletronico.java
+++ src/application/bank/AppCaixaEletronico.java
@@ -1,9 +1,7 @@
 package application.bank;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
@@ -22,43 +20,47 @@ public class AppCaixaEletronico {
 		System.out.println("Digite S para saque");
 		System.out.println("D para depósito");
 		System.out.println("T para transferências entre contas: ");
+		System.out.println("V para visualizar extrato das transações: ");
 		System.out.println("E para Sair: ");
-
+		List<String>historic = new ArrayList<>();
 		Conta conta12 = new Conta(1001, new Client("Bryan", 123), 1000.0, 200.0);
 		Conta conta13 = new Conta(1002, new Client("Sophie", 456), 500.0, 200.0);
-
-		while (true) {
-
+		
+		for (int i=0; i<=10; i++) {
 			char n = sc.next().charAt(0);
 				if (n == 'S') {
-
+					
 					System.out.println("Digite o valor a ser sacado: ");
 					double x = sc.nextDouble();
 					conta12.saque(x);
-					System.out.println(conta12);
+//					System.out.println(conta12);
 				}
 
 				else if (n == 'D') {
 					System.out.println("Digite o valor do depósito: ");
 					double y = sc.nextDouble();
 					conta12.deposito(y);
-					System.out.println(conta12);
+//					System.out.println(conta12);
 				}
 
 				else if (n == 'T') {
 					System.out.println("Digite o valor a ser transferido: ");
 					double valor = sc.nextDouble();
 					conta12.transferir(conta13, valor);
-					System.out.println(conta12);
-					System.out.println(conta13);
+//					System.out.println(conta12);
+//					System.out.println(conta13);
 				}
 
 				else if (n == 'E') {
 					System.out.println("Extrato resumido: ");
 					System.out.println(conta12);
 				}
-
+				else if(n == 'V') {
+					System.out.println(historic);
+				}
+				historic.add(conta12.toString());
 			}
+		sc.close();
 		}
 
 	}
